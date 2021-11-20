e8874ce6f22296137e067e38c134a33812ea8962
Abdullah Al-Nawfal <a.alnawfal.1992@gmail.com>
2021-11-20 05:37:15 +0300

    Update DBConnectio.java

--- DBConnectio.java
+++ DBConnectio.java
@@ -8,7 +8,6 @@ public class DataBase {
     static String user="root";
     static String Pass="root";
     static String ServerName="jdbc:sqlserver://DESKTOP-9OGLLHH\\SQLEXPRESS;databaseName=Allele_Freq";
-    //static String DBName="Allele_Freq";
     static Connection connection;
     
     public static void main(String[] args) throws SQLException 
